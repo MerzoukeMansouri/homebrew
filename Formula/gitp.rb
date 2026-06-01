4ea938e6ca9e290b1722dea89dfa87f27d828f8d
Merzouke Mansouri <merzouke.mansouri@ext.adeo.com>
2026-06-01 11:44:54 +0200

    chore: initial formula placeholder

---
+++ Formula/gitp.rb
@@ -0,0 +1,10 @@
+class Gitp < Formula
+  desc "K9s-style TUI for managing git operations across multiple repositories"
+  homepage "https://github.com/MerzoukeMansouri/gitp"
+  version "0.0.0"
+  license "MIT"
+
+  def install
+    bin.install "gitp"
+  end
+end
